0ae6f2c9ecb35fa2aa475ce8bf2b0efb52ba25d5
Chahir Chalouati <chehhhir@gmail.com>
2023-05-14 10:47:54 +0200

    init project

---
+++ src/main/java/com/crcl/search/filtering/AdvancedSearchAndFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.crcl.search.filtering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSearchAndFilteringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedSearchAndFilteringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crcl/search/filtering/AdvancedSearchAndFilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crcl.search.filtering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedSearchAndFilteringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
